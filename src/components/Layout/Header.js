9e4f787655851842640017e2b844c939b3fb412b
Kent Jakobsson <kentegel@gmail.com>
2021-11-16 18:17:31 +0100

    added css stylings

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,14 +1,22 @@
 import { Fragment } from 'react';
 
 import HeaderCartButton from './HeaderCartButton';
-import mealsImage from '../../Assets/meals.jpg';
+import mealsImage from '../../Assets/pexels-jonathan-borba-2983101.jpg';
 import classes from './Header.module.css';
+import imonitLogo from './imonit3.png';
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>ReactMeals</h1>
+        <h1 className={classes['logo-text']}>
+          <img
+            className={classes['imonit-logo']}
+            src={imonitLogo}
+            alt="imonit"
+          ></img>
+          Imont it Meals
+        </h1>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
